119d0a3658e93acf383d3c132ecfb7aa58644216
RoKiShii <fzq2061627258@gmail.com>
2026-04-10 00:06:05 +0800

    rollback

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -1,4 +1,4 @@
-﻿<script setup lang="ts">
+﻿﻿﻿<script setup lang="ts">
 import { computed } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
@@ -196,4 +196,3 @@ const logout = () => {
   }
 }
 </style>
-
